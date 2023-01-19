85fb208b46989559767199e6dfacf496db954592
Babadu31 <bastienmiss31@gmail.com>
2023-01-19 06:38:51 -0800

    6469

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker run --rm --interactive --volume=$(pwd):/app --workdir=/app ubuntu:18.04 /bin/bash
-sudo apt-get update && sudo apt-get install -y hugo make
-sudo curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o last.deb
-sudo apt install ./last.deb
-sudo rm last.deb
+apt-get update && sudo apt-get install -y hugo make
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o last.deb
+apt install ./last.deb
+rm last.deb
 make build
